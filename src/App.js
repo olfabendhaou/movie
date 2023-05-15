bf7eb4a23cbd1696e19e6b60773b88dad35e08f7
olfabendhaou <olfa.bendhaou.gmc@gmail.com>
2023-05-15 01:40:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
+import moviesData from './component/Data/Data'
+import MovieList from './component/MovieList/MovieList'
+import AddMovie from './component/AddMovie/AddMovie'
+import Filter from './component/Filter/Filter';
 function App() {
+  const [imputSearch,setImputSearch] = useState("")
+  const [movies, setMovies]=useState(moviesData)
+  const add =(newMovie) => {setMovies({...movies,newMovie})}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Filter imputSearch={imputSearch} setImputSearch={setImputSearch}/>
+    <MovieList movies={movies} imputSearch={imputSearch} />
+    <AddMovie add={add}  />
+  </div>
   );
 }
 
